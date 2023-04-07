4a6bbad861f5c5dc570ca39fb911bd3e9e4cbd4c
Madeline <madelinejgowan@gmail.com>
2023-04-07 10:21:13 -0700

    10:21AM

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Landing.css';
-import {Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 import heroImg from '../../Assets/image 10.png';
 import tavernImg from '../../Assets/Lets get started.png';
 import cardOneImg from '../../Assets/Rectangle 6.png';
@@ -9,7 +9,39 @@ import cardTwoImg from '../../Assets/Rectangle 7.png';
 export default function Landing() {
   return (
     <div>
-      <img className='heroImg' src={heroImg}/>
+      <Row>
+      <img className='heroImg' src={heroImg} />
+      </Row>
+      <br/>
+      <Row>
+        <Col className='col-6'>
+          <img className='tavernImg' src={tavernImg} />
+        </Col>
+
+        <Col>
+          <Card style={{ width: '18rem' }} className='cardOne'>
+            <Card.Img variant="top" src={cardOneImg} className='cardImg'/>
+            <Card.Body>
+              <Card.Title>An easy way to connect locally with your fellow adventurers</Card.Title>
+              <Card.Text>
+              Find people in your area looking to delve into a fantastic journey, or leap head first into a dungeon! 
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col>
+        <Card style={{ width: '18rem' }} className='cardTwo'>
+            <Card.Img variant="top" src={cardTwoImg} className='cardImg'/>
+            <Card.Body>
+              <Card.Title>Create the ultimate campaign and let the heroes find you!</Card.Title>
+              <Card.Text>
+              There are heroes near you looking for their next campaign run, or maybe even their first!
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </div>
   )
 }
