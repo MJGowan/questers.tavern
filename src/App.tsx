f62b96a5afc1a04027707782abcce7e4491070fb
Madeline <madelinejgowan@gmail.com>
2023-04-14 16:07:26 -0700

    Responsiveness 4:07PM

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,13 @@ import React, { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
-  const logo = require('./Assets/logo.jpg');
-  let loggedIn = false;
-  // use props to display username and profile pic when people are logged in
+
   return (
     <div>
-        <Row className='navbarBg'>
-          <Col className='col-7'>
-            <img src={logo} />
-          </Col>
-          <Col className='navMargin'>
-            <p className='doubleFont navText'>Map</p>
-          </Col>
-          <Col className='navMargin'>
-            <p className='doubleFont navText'>FAQs</p>
-          </Col>
-          <Col className='navMargin'>
-            <p className='doubleFont navText'>D&D Website</p>
-          </Col>
-          {
-            loggedIn ? (
-              <Col>
-                {/* props.username to display the active user */}
-                <p className='doubleFont navText'></p>
-              </Col>
-            ) : (
-              <Col>
-                <p className='doubleFont navText'>Sign Up/Login</p>
-              </Col>
-            )
-          }
-        </Row>
+        <Navbar/>
     </div>
   );
 
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function Navbar() {
+  const logo = require('../../Assets/logo.jpg');
+  let loggedIn = false;
+
+  const [isHamburger, setIsHamburger] = useState('');
+  useEffect(() => {
+    const handleChange = () => {
+      if (window.innerWidth <= 1024 && window.innerWidth >= 769) {
+        setIsHamburger('hamburger1');
+      } else if (window.innerWidth <= 768) {
+        setIsHamburger('hamburger2')
+      } else {
+        setIsHamburger('no')
+      }
+    };
+    handleChange();
+    window.addEventListener("resize", handleChange);
+    return () => window.removeEventListener("resize", handleChange);
+  }, [])
+
+  // use props to display username and profile pic when people are logged in
+  return (
+    <div>
+      {
+        isHamburger === 'hamburger1' ? (
+          <div>
+            <Row className='navbarBg'>
+              <Col className='col-6'>
+                <img src={logo} />
+              </Col>
+              <Col className='d-flex justify-content-end'>
+                {
+                  window.innerWidth <= 700 ? (
+                    <MenuIcon fontSize="large" style={{marginTop: '14%', marginRight: '5%', color: 'rgb(57, 86, 57)'}}/>
+                  ) : (
+                    <MenuIcon fontSize="large" style={{marginTop: '7%', marginRight: '5%', color: 'rgb(57, 86, 57)'}}/>
+                  )
+                }
+              
+              </Col>
+              {
+                // loggedIn ? (
+                //   <Col>
+                //     {/* props.username to display the active user */}
+                //     <p className='doubleFont navText'></p>
+                //   </Col>
+                // ) : (
+                //   <Col>
+                //     <p className='doubleFont navText'>Sign Up/Login</p>
+                //   </Col>
+                // )
+              }
+            </Row >
+          </div>
+        ) : (
+          <div></div>
+        ) : (
+          <div>
+            <Row className='navbarBg'>
+              <Col className='col-7'>
+                <img src={logo} />
+              </Col>
+              <Col className='navMargin'>
+                <p className='doubleFont navText'>Map</p>
+              </Col>
+              <Col className='navMargin'>
+                <p className='doubleFont navText'>FAQs</p>
+              </Col>
+              <Col className='navMargin'>
+                <p className='doubleFont navText'>D&D Website</p>
+              </Col>
+              {
+                // loggedIn ? (
+                //   <Col>
+                //     {/* props.username to display the active user */}
+                //     <p className='doubleFont navText'></p>
+                //   </Col>
+                // ) : (
+                //   <Col>
+                //     <p className='doubleFont navText'>Sign Up/Login</p>
+                //   </Col>
+                // )
+              }
+            </Row >
+          </div >
+        )
+      }
+
+    </div >
+  )
+}
