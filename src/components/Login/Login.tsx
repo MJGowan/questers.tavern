9ac872f5a846fd63f723c2ffa1552fd773b58b27
Madeline <madelinejgowan@gmail.com>
2023-04-27 11:01:38 -0700

    Login Responsiveness 11:01AM

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 import './Login.css';
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import bg from '../../Assets/LoginBg.png';
 
 export default function Login() {
+
+// const [screen, setScreen] = useState('');
+// useEffect(() => {
+//   const handleChange = () => {
+//     if (window.innerWidth <= 1024 && window.innerWidth >= 769) {
+//       setScreen('laptop')
+//     } else if (window.innerWidth <= 768 && window.innerWidth >= 426) {
+//       setScreen('tablet')
+//     } else if (window.innerWidth <= 425) {
+//       setScreen('mobile')
+//     } else {
+//       setScreen('desktop')
+//     }
+//   }
+//   handleChange();
+//   window.addEventListener("resize", handleChange);
+//   return () => window.removeEventListener("resize", handleChange)
+// }, [])
+
   return (
-    <div className='bgImg doubleFont white'>
+    <div className='doubleFont white'>
+      <img src={bg} className='bgImg'/>
       <Row>
         <Col className='col-2'></Col>
         <Col className='col-8'>
+          <br/>
           <br/>
           <Container className='shadowBox'>
             <br/>
@@ -23,7 +45,11 @@ export default function Login() {
                   <p>Email:</p>
                 </Row>
                 <Row>
-                  <input></input>
+                  <Col></Col>
+                  <Col>
+                  <input type='email' className='input'></input>
+                  </Col>
+                  <Col></Col>
                 </Row>
               </Col>
             </Row>
@@ -34,7 +60,11 @@ export default function Login() {
                   <p>Password:</p>
                 </Row>
                 <Row>
-                  <input></input>
+                  <Col></Col>
+                <Col className='d-flex justify-content-center'>
+                  <input type='password' className='input'></input>
+                  </Col>
+                  <Col></Col>
                 </Row>
               </Col>
             </Row>
