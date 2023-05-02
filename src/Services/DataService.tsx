b10b16afb5a364f3c7761f3da3c49436d247434b
FreddySSF <fernandoaguilarssf@gmail.com>
2023-05-02 15:40:47 -0700

    CreateAccount function in dataservice

--- src/Services/DataService.tsx
+++ src/Services/DataService.tsx
@@ -5,3 +5,25 @@ export default function Filler() {
     <div>Filler</div>
   )
 }
+
+// Backend API Fetches
+
+async function CreateAccount(createdUser: object) {
+  const result = await fetch('https://localhost:7276/User/AddUser', {
+      method: "POST",
+      headers: {
+          'Content-Type': "application/json"
+      },
+      body: JSON.stringify(createdUser)
+  });
+  if (!result.ok) {
+      const message = `An Error has Occured ${result.status}`;
+      throw new Error(message);
+  } else {
+  }
+  let data = await result.json();
+  console.log(data);
+  return data;
+}
+
+export { CreateAccount}
