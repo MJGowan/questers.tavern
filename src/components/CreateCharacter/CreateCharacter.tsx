ff40cb1e906f123446c06b3e932d90aaef39fd17
Madeline <madelinejgowan@gmail.com>
2023-05-10 14:35:19 -0700

    Character Creation 2:35PM

--- src/components/CreateCharacter/CreateCharacter.tsx
+++ src/components/CreateCharacter/CreateCharacter.tsx
@@ -1,7 +1,97 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './CreateCharacter.css';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 export default function CreateCharacter() {
+  const heroImg = require('../../Assets/image 13.png');
+  const [name, setName] = useState('');
+  const [level, setLevel] = useState('');
+  const [desc, setDesc] = useState('');
+  const [charImg, setCharImg] = useState('');
+  const [race, setRace] = useState('');
+  const [charClass, setCharClass] = useState('');
+  const [background, setBackground] = useState('');
+  const [alignment, setAlignment] = useState('');
+
   return (
-    <div>CreateCharacter</div>
+    <div className='bg'>
+      <div>
+        <Row>
+          <Col>
+            <img src={heroImg} style={{ width: '100vw' }}></img>
+            <p className='title doubleFont'>CHARACTER CREATOR</p>
+          </Col>
+        </Row>
+      </div>
+      <Container className='darkBox'>
+        <Row>
+          <Row>
+            <Col className='col-3'>
+              <p>Character Image:</p>
+              <Form.Group controlId="formFile" className="mb-3">
+                <Form.Control type="file" />
+              </Form.Group>
+            </Col>
+            <Col className='col-3'>
+              <p>Name:</p>
+              <input className='nameInput input mb-3' placeholder='Name'></input>
+              <p>Level:</p>
+              <input className='lvlInput input' placeholder='Level 1-20'></input>
+            </Col>
+            <Col>
+            <p>Race:</p>
+              <Form.Select aria-label="Default select example">
+                <option>Race</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </Form.Select>
+            </Col>
+            <Col>
+            <p>Class:</p>
+              <Form.Select aria-label="Default select example">
+                <option>Class</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </Form.Select>
+            </Col>
+          </Row>
+          <Row className='mt-5'>
+            <Col className='col-6'>
+              <p>Character Description:</p>
+              <input className='desc input'></input>
+            </Col>
+            <Col>
+            <p>Background:</p>
+              <Form.Select aria-label="Default select example">
+                <option>Background</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </Form.Select>
+            </Col>
+            <Col>
+            <p>Alignment:</p>
+              <Form.Select aria-label="Default select example">
+                <option>Alignment</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </Form.Select>
+            </Col>
+          </Row>
+          <Row className='mt-5'>
+            <Col className='col-9'></Col>
+            <Col>
+              <Button className='contBtn btn'>Continue</Button>
+            </Col>
+            <Col>
+              <Button className='backBtn btn'>Back</Button>
+            </Col>
+          </Row>
+        </Row>
+      </Container>
+    </div>
   )
 }
