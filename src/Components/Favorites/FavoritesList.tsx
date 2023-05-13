fa3e8f8544076b4efdeaf72c5e161158efde35c6
chris martinex <uschrismartinez@gmail.com>
2023-05-13 12:05:08 -0700

    scroll and cards

--- src/Components/Favorites/FavoritesList.tsx
+++ src/Components/Favorites/FavoritesList.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import './Favorites.css'
+import { padding } from '@mui/system';
 
 export default function FavoritesList() {
   let navigate = useNavigate();
@@ -9,6 +10,7 @@ export default function FavoritesList() {
   const tavernmen = require("../../Assets/tavernmen.jpg");
   const profilepic = require("../../Assets/profilepic.png")
   const naruto = require("../../Assets/naruto.png")
+  const scroll = require("../../Assets/image 7.png")
 
   const username = "Username";
   const profilePic = require('../../Assets/Pic.png');
@@ -36,7 +38,9 @@ export default function FavoritesList() {
               </div>
               <div className='button-container d-none d-md-block'>
                 <div className='d-flex ml-auto'>
-                  <button className='button' onClick={() => navigate(-1)}>BACK</button>
+                  <button className='button' onClick={() => navigate(-1)}>
+                    BACK
+                  </button>
                 </div>
               </div>
             </div>
@@ -66,11 +70,15 @@ export default function FavoritesList() {
                 <p>{exp}</p>
               </Row>
               <Row>
-                <p onClick={() => navigate('/FavoritesList')}>Friends List: {numFriends}</p>
+                <p onClick={() => navigate("/FavoritesList")}>
+                  Friends List: {numFriends}
+                </p>
               </Row>
               <hr />
               <Row>
-                <p className='editBtn' onClick={editProfile}>Edit Profile</p>
+                <p className='editBtn' onClick={editProfile}>
+                  Edit Profile
+                </p>
               </Row>
             </Container>
           </div>
@@ -98,8 +106,9 @@ export default function FavoritesList() {
           >
             <Card.Header>
               <input
+                type="text"
                 style={{ width: "50%", borderRadius: "10px", height: "50px" }}
-                value='Search By UserName'
+                placeholder='Search By UserName'
               ></input>
               <button
                 style={{
@@ -117,61 +126,82 @@ export default function FavoritesList() {
             </Card.Header>
 
             <Card.Body>
-              <Card
+              <div
                 style={{
-                  width: "300px",
-                  height: "470px",
-                  border: "2px solid #FFA825",
+                  display: "flex",
+                  flexDirection: "row-reverse",
+                  padding: "10px",
+                  margin: "10px 0",
                 }}
               >
-                <Card.Body style={{ backgroundColor: "#4A423F" }}>
-                  <div className='parent'>
-                    <img src={naruto} alt='' className='logo-image1' />
-                  </div>
-                  <Card.Title className='profile'>
-                    Name: Naruto45
-                    <Card.Text>Full Name:</Card.Text>
-                    <Card.Text>Location:</Card.Text>
-                    <Card.Text>Campaigns:</Card.Text>
-                  </Card.Title>
+                <img
+                  src={scroll}
+                  alt=''
+                  style={{ width: "50%", height: "320px" }}
+                />
 
+                <Card
+                  style={{
+                    width: "300px",
+                    height: "420px",
+                    border: "2px solid #FFA825",
+                  }}
+                >
+                  <Card.Body style={{ backgroundColor: "#4A423F" }}>
+                    <div className='parent'>
+                      <img
+                        src={naruto}
+                        alt=''
+                        className='logo-image1'
+                        style={{ width: "50%", height: "auto" }}
+                      />
+                    </div>
+                    <Card.Title
+                      style={{ backgroundColor: "#4A423F", width: "100%" }}
+                      className='centered-text'
+                    >
+                      Name: Naruto45
+                      <Card.Text>Full Name:</Card.Text>
+                      <Card.Text>Location:</Card.Text>
+                      <Card.Text>Campaigns:</Card.Text>
+                    </Card.Title>
 
-                  <button
-                    className='cardButton'
-                    style={{
-                      width: "100%",
-                      margin: "10px",
-                      borderRadius: "10px",
-                      height: "50px",
-                      padding: "10px 20px",
-                      backgroundColor: "#4A423F",
-                      color: "white",
-                    }}
-                  >
-                    Add to Favorites
-                  </button>
-                  <button
-                    className='cardButton'
-                    style={{
-                      width: "100%",
-                      marginLeft: "10px",
-                      marginRight: "10px",
-                      borderRadius: "10px",
-                      height: "50px",
-                      padding: "10px 20px",
-                      backgroundColor: "#4A423F",
-                      color: "white",
-                    }}
-                  >
-                    Remove
-                  </button>
-                </Card.Body>
-              </Card>
+                    <button
+                      className='cardButton'
+                      style={{
+                        width: "100%",
+                        margin: "1px",
+                        borderRadius: "10px",
+                        height: "50px",
+                        padding: "10px 20px",
+                        backgroundColor: "#4A423F",
+                        color: "white",
+                      }}
+                    >
+                      Add to Favorites
+                    </button>
+                    <button
+                      className='cardButton'
+                      style={{
+                        width: "90%",
+                        marginLeft: "10px",
+                        marginRight: "10px",
+                        borderRadius: "10px",
+                        height: "50px",
+                        padding: "10px 20px",
+                        backgroundColor: "#4A423F",
+                        color: "white",
+                      }}
+                    >
+                      Remove
+                    </button>
+                  </Card.Body>
+                </Card>
+              </div>
             </Card.Body>
           </Card>
         </Col>
       </Row>
-
     </Container>
   );
 }
