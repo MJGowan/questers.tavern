7d44f7f234b96d37ce58519cccf7a2f7e1bec724
Madeline <madelinejgowan@gmail.com>
2023-05-15 11:23:42 -0700

    Responsiveness

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import './Navbar.css';
-import { Row, Col, Nav } from 'react-bootstrap';
+import { Container, Row, Col, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import MenuIcon from '@mui/icons-material/Menu';
-import Login from '../Login/Login';
+
 
 export default function Navbar() {
   const logo = require('../../Assets/logo.jpg');
-  let loggedIn = false;
   const [username, setUsername] = useState('');
-  const [login, setLoggedIn] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
 
   const [isHamburger, setIsHamburger] = useState('');
   useEffect(() => {
     const handleChange = () => {
-      if (window.innerWidth <= 1024 && window.innerWidth >= 769) {
-        setIsHamburger('hamburger1');
-      } else if (window.innerWidth <= 768) {
-        setIsHamburger('hamburger2')
+      if (window.innerWidth <= 1024) {
+        setIsHamburger('hamburger')
       } else {
         setIsHamburger('')
       }
@@ -29,68 +26,20 @@ export default function Navbar() {
 
   // use props to display username and profile pic when people are logged in
   return (
-    <div>
+    <Container fluid>
       {
-        isHamburger === 'hamburger1' ? (
+        isHamburger === 'hamburger' ? (
           <div>
             <Row className='navbarBg'>
               <Col className='col-6'>
-                <img src={logo} />
+                <img src={logo} className='logoImg'/>
               </Col>
               <Col className='d-flex justify-content-end'>
-                {
-                  window.innerWidth <= 700 ? (
-                    <MenuIcon fontSize="large" style={{ marginTop: '17%', marginRight: '5%', color: 'rgb(57, 86, 57)' }} />
-                  ) : (
-                    <MenuIcon fontSize="large" style={{ marginTop: '7%', marginRight: '5%', color: 'rgb(57, 86, 57)' }} />
-                  )
-                }
+              <MenuIcon fontSize="large" className='menuIcon' />
 
               </Col>
-              {
-                loggedIn ? (
-                  <Col>
-                    {/* props.username to display the active user */}
-                    <p className='doubleFont navText'></p>
-                  </Col>
-                ) : (
-                  <Col>
-                    <Nav.Link className='doubleFont navText'><span onClick={() => <Login />}>Sign Up/Login</span></Nav.Link>
-                  </Col>
-                )
-              }
             </Row>
           </div>
-        ) : isHamburger === 'hamburger2' ? (
-          <div>
-            <Row className='navbarBg'>
-              <Col className='col-7'>
-                <img src={logo} className='logo' />
-              </Col>
-              <Col className='d-flex justify-content-end'>
-                {
-                  window.innerWidth <= 700 ? (
-                    <MenuIcon fontSize="large" style={{ marginTop: '17%', marginRight: '5%', color: 'rgb(57, 86, 57)' }} />
-                  ) : (
-                    <MenuIcon fontSize="large" style={{ marginTop: '7%', marginRight: '5%', color: 'rgb(57, 86, 57)' }} />
-                  )
-                }
-
-              </Col>
-              {
-                loggedIn ? (
-                  <Col>
-                    {/* props.username to display the active user */}
-                    <p className='doubleFont navText'></p>
-                  </Col>
-                ) : (
-                  <Col>
-                    <Nav.Link className='doubleFont navText'><span onClick={() => <Login />}>Sign Up/Login</span></Nav.Link>
-                  </Col>
-                )
-              }
-            </Row >
-          </div >
         ) : (
           <div>
             <Row className='navbarBg'>
@@ -105,23 +54,17 @@ export default function Navbar() {
                 <Link to='/TavernBoard' className='navItems'>
                   Campaigns
                 </Link>
-
                 <Link to='/FAQs' className='navItems'>
                   FAQs
                 </Link>
-
                 <Link to="https://dnd.wizards.com/" className='navItems'>
                   D&D Website
                 </Link>
-
                 {
                   loggedIn ? (
                     <Link to='/Profile' className='navItems'>{username}</Link>
-
                   ) : (
-
                     <Link to='/Login' className='navItems'>Sign Up/Login</Link>
-
                   )
                 }
                   </div>
@@ -131,6 +74,6 @@ export default function Navbar() {
         )
       }
 
-    </div >
+    </Container >
   )
 }
